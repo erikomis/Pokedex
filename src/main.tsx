31f536d5812f3ac25079ff10a33016c357b3afc5
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-07-13 17:18:40 -0300

    feat: provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { GlobalStyle } from "./styles/global";
+import { MyThemeProvider } from "./provider/ThemeProvider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <MyThemeProvider>
+        <GlobalStyle />
+        <App />
+      </MyThemeProvider>
+    </Provider>
   </React.StrictMode>
-)
+);
